0859c78f57139db297ec415036ebeb144a2e4b0a
user2684 <user2684@users.noreply.github.com>
2019-08-30 23:31:20 +0200

    Added support for the new sensor registration

--- service/dht.py
+++ service/dht.py
@@ -71,4 +71,9 @@ class Dht(Service):
 
     # What to do when receiving a new/updated configuration for this module    
     def on_configuration(self,message):
-        pass
+        # register/unregister the sensor
+        if message.args.startswith("sensors/"):
+            if message.is_null: 
+                sensor_id = self.unregister_sensor(message)
+            else: 
+                sensor_id = self.register_sensor(message)
